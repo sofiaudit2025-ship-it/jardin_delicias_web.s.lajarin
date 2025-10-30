a796771836cda45d77d6f3e8732ba0ac533134e5
sofiaudit2025-ship-it <sofia.udit.2025@gmail.com>
2025-10-30 09:27:52 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let contador = 0;
+
+function carga() {
+    if (contador <= 100) {
+        document.getElementById("porcentaje").textContent = contador + "%";
+        contador++;
+        setTimeout(carga, 30); // velocidad de carga
+    } else {
+        // Transición suave al finalizar
+        document.getElementById("carga").style.top = "-100%";
+    }
+}
+
+window.onload = carga;
